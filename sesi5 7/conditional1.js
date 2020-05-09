a47bf096703691c1afbf44a59fa4f0c214ba6738
blue96 <pascalfelim@yahoo.com>
2020-05-09 11:04:08 +0700

    upload

---
+++ sesi5 7/conditional1.js
@@ -0,0 +1,14 @@
+let div = 'SOH';
+
+if (div === 'SOH'){
+    console.log('melayani kesehatan karyawan dan keluarga beserta pasien umum');
+}
+else if (div === 'SGA'){
+    console.log('meyediakan seluruh kebutuhan umum PT Inalum');
+}
+else if(div === 'HR'){
+    console.log('melakukan rekrutmen untuk calon pegawai baru jika diperlukan')
+}
+else {
+    console.log('Seksi tidak ditemukan');
+}
