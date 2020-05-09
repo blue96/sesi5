fbec2f3e3584a950bb31ca3fb7f9c2659f2c89d2
blue96 <pascalfelim@yahoo.com>
2020-05-09 10:22:38 +0700

    upload

--- sesi5 2/syntax.js
+++ sesi5 2/syntax.js
@@ -5,5 +5,6 @@ do{
     let asalNegara = prompt('Masukkan Asal Negara');
     konfirmasi = confirm('apakah data yang di isi sudah benar?');
 } while (konfirmasi===false)
-
+console.log(namaUser);
+console.log(asalNegara);
 alert('terima kasih sudah mengisi form');
